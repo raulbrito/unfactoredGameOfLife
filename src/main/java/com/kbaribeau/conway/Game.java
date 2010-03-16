31244d0d12895fe3d227dc131b3fe99d73f086ef
Raul Brito <raul.brito@gmail.com>
2010-03-16 07:31:08 +0100

    started refactoring

--- src/main/java/com/kbaribeau/conway/Game.java
+++ src/main/java/com/kbaribeau/conway/Game.java
@@ -6,12 +6,20 @@ public class Game {
 	public boolean isAlive(int y, int x) {
 		return grid[y][x];
 	}
+	
+	private void setAlive(int y, int x, boolean alive) {
+		grid[y][x] = alive;
+	}
 
 	public Game(String start) {
 		int sizeX = start.indexOf("\n");
 		int sizeY = start.split("\n").length;
 		grid = new boolean[sizeY][sizeX];
 
+		buildTable(start);
+	}
+
+	private void buildTable(String start) {
 		int x = 0, y = 0;
 		for (char c : start.toCharArray()) {
 			switch (c) {
@@ -20,10 +28,10 @@ public class Game {
 					x = 0;
 					continue;
 				case '.':
-					grid[y][x] = false;
+					this.setAlive(y, x, false);
 					break;
 				case 'X':
-					grid[y][x] = true;
+					this.setAlive(y, x, true);
 					break;
 			}
 			x++;
@@ -33,28 +41,19 @@ public class Game {
 
 	public void tick() {
 		Game newGame = new Game(toString());
-		for (int y = 0; y < newGame.grid.length; y++) {
-			for (int x = 0; x < newGame.grid[0].length; x++) {
-				int neighborCount = 0;
-
-				if (hasLife(y - 1, x - 1)) neighborCount++;
-				if (hasLife(y, x - 1)) neighborCount++;
-				if (hasLife(y + 1, x - 1)) neighborCount++;
-				if (hasLife(y - 1, x)) neighborCount++;
-				if (hasLife(y + 1, x)) neighborCount++;
-				if (hasLife(y - 1, x + 1)) neighborCount++;
-				if (hasLife(y, x + 1)) neighborCount++;
-				if (hasLife(y + 1, x + 1)) neighborCount++;
-
-
+		for (int y = 0; y < gridRowLength(newGame); y++) {
+			for (int x = 0; x < gridColumnLength(newGame); x++) {
+				
+				int neighborCount = countNumberOfNeighbours(y, x);
+				
 				if (neighborCount == 1 || neighborCount == 0) {
-					newGame.grid[y][x] = false;
+					newGame.setAlive(y, x, false);
 				}
 				if (neighborCount > 3) {
-					newGame.grid[y][x] = false;
+					newGame.setAlive(y, x, false);
 				}
 				if (neighborCount == 3) {
-					newGame.grid[y][x] = true;
+					newGame.setAlive(y, x, true);
 				}
 			}
 		}
@@ -62,8 +61,33 @@ public class Game {
 		this.grid = newGame.grid;
 	}
 
+	private int countNumberOfNeighbours(int y, int x) {
+		int neighborCount = 0;
+
+		if (hasLife(y - 1, x - 1)) neighborCount++;
+		if (hasLife(y, x - 1)) neighborCount++;
+		if (hasLife(y + 1, x - 1)) neighborCount++;
+		if (hasLife(y - 1, x)) neighborCount++;
+		if (hasLife(y + 1, x)) neighborCount++;
+		if (hasLife(y - 1, x + 1)) neighborCount++;
+		if (hasLife(y, x + 1)) neighborCount++;
+		if (hasLife(y + 1, x + 1)) neighborCount++;
+
+		return neighborCount;
+	}
+
+	private int gridColumnLength(Game game) {
+		return game.grid[0].length;
+	}
+
+	private int gridRowLength(Game game) {
+		return game.grid.length;
+	}
+
 	private boolean hasLife(int y, int x) {
-		return !(y < 0 || y >= grid.length) && !(x < 0 || x >= grid[0].length) && grid[y][x];
+		boolean rowExists = (y >= 0 && y < gridRowLength(this));
+		boolean columnExists = (x >= 0 && x < gridColumnLength(this));
+		return rowExists && columnExists && isAlive(y, x);
 	}
 
 	@Override
@@ -78,4 +102,6 @@ public class Game {
 
 		return result.deleteCharAt(result.lastIndexOf("\n")).toString();
 	}
+	
+	
 }
